4ec6278c42cc5e46ffcbcf65baff0b1c0223d7f8
lukejans <91230392+lukejans@users.noreply.github.com>
2022-12-18 22:32:26 -0500

    Added Event Listeners To Sketch

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-// parent
+// sketch pad container
 const sketchPad = document.getElementById('sketchPad');
-sketchPad.setAttribute('style', '');
+sketchPad.setAttribute(
+  'style',
+  'display: grid; grid-template-columns: repeat(16, 1fr); grid-template-rows: repeat(16, 1fr); padding: 2px; min-height: 400px;'
+);
+
 // build sketch pad
 for (let i = 0; i < 256; i++) {
   const displayBlock = document.createElement('div');
   displayBlock.setAttribute(
     'style',
-    ' border: 1px solid white; height: 25px; width: 25px; background: rgb(24, 24, 24);'
+    'height: 25px; width: 25px; background: rgb(24, 24, 24);'
   );
   sketchPad.appendChild(displayBlock);
+
+  displayBlock.addEventListener('mouseover', function () {
+    displayBlock.style.backgroundColor = 'red';
+  });
 }
