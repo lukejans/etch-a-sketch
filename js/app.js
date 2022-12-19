ddee7d7f2ae53b6a27c831c020a3fb1b047aa287
lukejans <91230392+lukejans@users.noreply.github.com>
2022-12-19 02:36:59 -0500

    Fixed Div Sizing

--- js/app.js
+++ js/app.js
@@ -2,16 +2,16 @@
 const sketchPad = document.getElementById('sketchPad');
 sketchPad.setAttribute(
   'style',
-  'display: grid; grid-template-columns: repeat(16, 1fr); grid-template-rows: repeat(16, 1fr); padding: 2px; min-height: 400px;'
+  'display: grid; grid-template-columns: repeat(16, 1fr); grid-template-rows: repeat(16, 1fr); padding: 2px; min-height: 402px;'
 );
 
 // build sketch pad
-function resolution(pixels, width) {
+function resolution(pixels) {
   for (let i = 0; i < pixels; i++) {
     const displayBlock = document.createElement('div');
     displayBlock.setAttribute(
       `style`,
-      `height: ${width}px; width: ${width}px; background: rgb(24, 24, 24);`
+      `height: 100%; width: 100%; background: rgb(24, 24, 24);`
     );
     sketchPad.appendChild(displayBlock);
 
@@ -19,8 +19,83 @@ function resolution(pixels, width) {
 
     displayBlock.addEventListener('mouseover', function () {
       displayBlock.style.backgroundColor = 'white';
-      opacity += 0.1;
+      opacity += 0.15;
       displayBlock.style.opacity = opacity;
     });
   }
+}
+
+// buttons container
+const ctrlBtns = document.getElementById('ctrlBtns');
+ctrlBtns.setAttribute(
+  'style',
+  'display: flex; gap: 20px; justify-content: center; align-items: center;'
+);
+
+// size buttons container
+const sizeBtns = document.createElement('div');
+ctrlBtns.appendChild(sizeBtns);
+sizeBtns.setAttribute(
+  'style',
+  'display: flex; gap: 10px; align-items: center; justify-content: center;'
+);
+
+// size buttons
+let sizeSwitch = 0;
+
+for (let i = 0; i < 3; i++) {
+  const pickSize = document.createElement('button');
+  sizeBtns.appendChild(pickSize);
+  pickSize.setAttribute(
+    'style',
+    'width: 100px; height: 20px; cursor: pointer; background-color: white; border: none; font-family: monospace; margin: 10px;'
+  );
+  const sizes = ['size 1', 'size 2', 'size 3'];
+  pickSize.textContent = `${sizes[sizeSwitch]}`;
+  sizeSwitch++;
+  pickSize.id = `size-${sizeSwitch}`;
+
+  pickSize.addEventListener('mouseover', function () {
+    pickSize.style.backgroundColor = 'red';
+  });
+  pickSize.addEventListener('mouseout', function () {
+    pickSize.style.backgroundColor = 'white';
+  });
+}
+
+//  clear the grid
+function clearGrid() {
+  while (sketchPad.firstChild) {
+    sketchPad.removeChild(sketchPad.firstChild);
+  }
+}
+
+// change grid size
+const sizeButtons = document.getElementsByTagName('button');
+
+for (let i = 0; i < sizeButtons.length; i++) {
+  sizeButtons[i].addEventListener('click', function () {
+    if (this.id === 'size-1') {
+      sketchPad.setAttribute(
+        'style',
+        'display: grid; grid-template-columns: repeat(16, 1fr); grid-template-rows: repeat(16, 1fr); min-height: 402px;'
+      );
+      clearGrid();
+      resolution(256);
+    } else if (this.id === 'size-2') {
+      sketchPad.setAttribute(
+        'style',
+        'display: grid; grid-template-columns: repeat(32, 1fr); grid-template-rows: repeat(32, 1fr); min-height: 402px;'
+      );
+      clearGrid();
+      resolution(1024);
+    } else if (this.id === 'size-3') {
+      sketchPad.setAttribute(
+        'style',
+        'display: grid; grid-template-columns: repeat(64, 1fr); grid-template-rows: repeat(64, 1fr); min-height: 402px;'
+      );
+      clearGrid();
+      resolution(4096);
+    }
+  });
 }
