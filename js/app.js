7acc367625a2b81ee0603b03c17f6e3b29fcd718
lukejans <91230392+lukejans@users.noreply.github.com>
2022-12-19 15:22:06 -0500

    Added Detailed Comments

--- js/app.js
+++ js/app.js
@@ -1,17 +1,16 @@
+// CONTAINERS //
 // sketch pad container
 const sketchPad = document.getElementById('sketchPad');
 sketchPad.setAttribute(
   'style',
   'display: grid; grid-template-columns: repeat(16, 1fr); grid-template-rows: repeat(16, 1fr); padding: 2px;'
 );
-
 // buttons container
 const ctrlBtns = document.getElementById('ctrlBtns');
 ctrlBtns.setAttribute(
   'style',
   'display: flex; gap: 20px; justify-content: center; align-items: center;'
 );
-
 // size buttons container
 const sizeBtns = document.createElement('div');
 ctrlBtns.appendChild(sizeBtns);
@@ -19,10 +18,12 @@ sizeBtns.setAttribute(
   'style',
   'display: flex; gap: 10px; align-items: center; justify-content: center;'
 );
+// COLOR BUTTONS //
 
-// size buttons
+// SIZE BUTTONS //
+// loops 3 times to create the 3 different size buttons
+// giving them style and all their own ids and text context
 let sizeSwitch = 0;
-
 for (let i = 0; i < 3; i++) {
   const pickSize = document.createElement('button');
   sizeBtns.appendChild(pickSize);
@@ -43,14 +44,20 @@ for (let i = 0; i < 3; i++) {
   });
 }
 
-//  clear the grid
+//  CLEAR THE GRID  //
+// loops around sketch pad grid removing each
+// child div created until none are left
 function clearGrid() {
   while (sketchPad.firstChild) {
     sketchPad.removeChild(sketchPad.firstChild);
   }
 }
 
-// build sketch pad
+// BUILD THE SKETCH PAD //
+// this function is called the argument as number
+// of pixels, it loops around until all of the boxes
+// have been created and added to the sketchPad container
+// then calls the draw() function passing displayBlock
 function resolution(pixels) {
   for (let i = 0; i < pixels; i++) {
     const displayBlock = document.createElement('div');
@@ -62,20 +69,18 @@ function resolution(pixels) {
     draw(displayBlock);
   }
 }
-
-// draw
+// DRAW ON MOUSE CLICK //
+// create a variable to set drawing to true or false
 let drawing = false;
-console.log(drawing);
-
+// setting draw to  true
 document.addEventListener('mousedown', function () {
   drawing = true;
-  console.log(drawing);
 });
+// resetting back to false
 document.addEventListener('mouseup', function () {
   drawing = false;
-  console.log(drawing);
 });
-
+// color blocks when true
 function draw(display) {
   display.addEventListener('mouseover', function () {
     if (drawing === true) {
@@ -84,9 +89,13 @@ function draw(display) {
   });
 }
 
-// change grid size
+// CHANGE THE GRID SIZE //
+// size buttons represents each size you can set the grid to and
+// listens for a click and depending on the id of the button a specific
+// grid style is displayed then calls the clearGrid() function to remove
+// any previously create elements and finally calls the resolution()
+// function to set the number of div's to appear on screen
 const sizeButtons = document.getElementsByTagName('button');
-
 for (let i = 0; i < sizeButtons.length; i++) {
   sizeButtons[i].addEventListener('click', function () {
     if (this.id === 'size-1') {
